fe9992fb7ead9d287973d10fc6634b98c06d33e7
EliasSamat <fachisamat@gmail.com>
2023-06-22 17:10:44 -0300

    ahora busca

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import './App.css';
-import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import personajes from './data.js';
-import characters, { Rick } from './data.js';
+//import Card from './components/Card.jsx';
+//import SearchBar from './components/SearchBar.jsx';
+// import personajes from './data.js';
+// import characters, { Rick } from './data.js';
+ import axios from 'axios';
+import React,{useState} from 'react';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
-   return (
+   const example = {
+      id: 1,
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      species: 'Human',
+      gender: 'Male',
+      origin: {
+         name: 'Earth (C-137)',
+         url: 'https://rickandmortyapi.com/api/location/1',
+      },
+      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+   };
+   const [characters, setCharacters] = useState([example]);
+   function onSearch(id) {
+      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+      return (
       <div className='App'>
-         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
+         
+        <SearchBar onSearch={onSearch}/> 
          <Cards characters={characters} />
-         <Card
+         {/* <Card
             id={personajes.id}
             name={personajes.name}
             status={personajes.status}
@@ -19,7 +46,7 @@ function App() {
             origin={Rick.origin.name}
             image={personajes.image}
             onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
+         /> */}
       </div>
    );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,17 @@
+import React,{useState} from "react";
+
 export default function SearchBar(props) {
+   const [ID, setID] = useState("") ; 
+   
+   const handleChange = (event) => {
+    setID(event.target.value);
+   } ; 
+     
    return (
       <div>
-          <input type='search' />
-         <button onClick={()=>props.onSearch(123)}>Agregar</button> 
+          <input type='search'  value={ID} onChange={handleChange}  />
+         <button onClick={()=>props.onSearch(ID)}>Agregar</button> 
+         <p>Valor actual del estado id: {ID}</p>  
       </div>
    );
 }
